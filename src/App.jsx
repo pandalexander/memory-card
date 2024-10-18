da0c8d043497139a3b6c85c9baabddf21cf6a89f
Alex Seeley <alexseeley134@gmail.com>
2024-10-18 11:34:59 -0600

    Remove vite default styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="w-screen flex flex-col justify-center items-center">
+      <div className="w-auto flex flex-col justify-center items-center">
         <ul className="grid grid-cols-4 gap-6">
           <ImageFetcher></ImageFetcher>
         </ul>
